6a897e9145d6322ae492bc81e950b52be2427dda
Vanika Sahni <141622433+sahnivanika@users.noreply.github.com>
2024-11-22 16:30:14 +0530

    Update blog-controller.js

--- controllers/blog-controller.js
+++ controllers/blog-controller.js
@@ -20,6 +20,12 @@ export const getAllBlogs = async (req, res, next) => {
 // Add a new blog
 export const addBlog = async (req, res, next) => {
     const { title, description, image, user } = req.body;
+
+    // Validate required fields
+    if (!title || !description || !user) {
+        return res.status(400).json({ message: "Title, description, and user are required" });
+    }
+
     let existingUser;
     try {
         existingUser = await User.findById(user);
@@ -28,15 +34,16 @@ export const addBlog = async (req, res, next) => {
         return res.status(500).json({ message: "Error finding user" });
     }
     if (!existingUser) {
-        return res.status(400).json({ message: "Unable to Find User by this id" });
+        return res.status(400).json({ message: "Unable to find user by this ID" });
     }
 
     const blog = new Blog({
         title,
         description,
-        image,
+        image: image || "", // Use an empty string if no image is provided
         user,
     });
+
     try {
         const session = await mongoose.startSession();
         session.startTransaction();
@@ -53,14 +60,18 @@ export const addBlog = async (req, res, next) => {
 
 // Update a blog by ID
 export const updateBlog = async (req, res, next) => {
-    const { title, description } = req.body;
+    const { title, description, image } = req.body;
     const blogId = req.params.id;
     let blog;
+
+    // Update fields only if provided
+    const updateData = {};
+    if (title) updateData.title = title;
+    if (description) updateData.description = description;
+    if (image) updateData.image = image;
+
     try {
-        blog = await Blog.findByIdAndUpdate(blogId, {
-            title,
-            description,
-        }, { new: true });
+        blog = await Blog.findByIdAndUpdate(blogId, updateData, { new: true });
     } catch (err) {
         console.log(err);
         return res.status(500).json({ message: "Unable to update the blog" });
@@ -100,23 +111,4 @@ export const getBlogsByUserId = async (req, res, next) => {
     if (!userBlogs || userBlogs.blogs.length === 0) {
         return res.status(404).json({ message: "No blogs found for this user" });
     }
-    return res.status(200).json({ user: userBlogs });
-};
-
-// Delete a blog by ID
-export const deleteBlog = async (req, res, next) => {
-    const id = req.params.id;
-    let blog;
-    try {
-        blog = await Blog.findByIdAndDelete(id).populate('user');
-        if (!blog) {
-            return res.status(404).json({ message: "Blog not found" });
-        }
-        await blog.user.blogs.pull(blog);
-        await blog.user.save();
-    } catch (err) {
-        console.log("Error during deletion:", err);
-        return res.status(500).json({ message: "Unable to delete blog" });
-    }
-    return res.status(200).json({ message: "Successfully Deleted" });
-};
+ 
